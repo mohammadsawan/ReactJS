6bd004f9ca501a3eb73b0a0418a92d2161e0938d
MohammadSawan <kinggamerads@gmail.com>
2022-03-17 16:46:55 +0200

    Update indexjs

--- Calculator/src/index.js
+++ Calculator/src/index.js
@@ -2,15 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />} path="/"></Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
